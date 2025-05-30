59f36be939860c5621d8ca9f7aa20e28a8123564
Zax71 <67716263+zax71@users.noreply.github.com>
2025-05-30 18:24:37 +0100

    Add component

---
+++ src/lib/what3words.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div class="">
+	<p class="slashes">///</p>
+	<p class="location">{@render children()}</p>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap');
+	* {
+		--what3words-red: #e11f26;
+	}
+	div {
+		padding: 6px 10px;
+		box-shadow: inset 0px -3px 0px var(--what3words-red);
+		display: flex;
+		width: fit-content;
+		background-color: white;
+	}
+
+	div > .slashes {
+		color: var(--what3words-red);
+	}
+
+	div > .location {
+		color: black;
+	}
+
+	p {
+		font-family: 'Source Sans 3', sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 600;
+		font-style: normal;
+		padding: 0;
+		margin: 0;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import What3words from '$lib/what3words.svelte';
+</script>
+
+<h1>what3words-svelte</h1>
+
+<What3words>filled.count.soap</What3words>
